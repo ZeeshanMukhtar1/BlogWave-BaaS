83cee36532575f682763053d3097d354cfa174a6
Zeeshan Mukhtar <Zeshanmukhtar0878@gmail.com>
2024-01-15 21:01:07 -0800

    💄  Refactor AllPosts component styling

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -15,14 +15,17 @@ function AllPosts() {
   }, []);
 
   return (
-    <div className="w-full py-8">
+    <div
+      style={{ backgroundColor: 'var(--bg-color)', color: 'var(--text-color)' }}
+      className="w-full py-8"
+    >
       <Container>
         {posts.length === 0 ? (
           <div className="flex flex-col items-center justify-center h-full py-16">
-            <h1 className="text-4xl font-bold mb-4 text-center">
+            <h1 className="mb-4 text-4xl font-bold text-center">
               404 - No Posts Found
             </h1>
-            <p className="text-lg mb-8 text-gray-600">
+            <p className="mb-8 text-lg text-white">
               It seems there are no posts available. Users might not have posted
               anything yet.
             </p>
@@ -31,7 +34,7 @@ function AllPosts() {
         ) : (
           <div className="flex flex-wrap">
             {posts.map((post) => (
-              <div key={post.$id} className="p-2 w-1/4">
+              <div key={post.$id} className="w-1/4 p-2">
                 <PostCard {...post} />
               </div>
             ))}
