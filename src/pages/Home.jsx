c46fe69eda3386866e0c21317d332c983070e4b9
Zeeshan Mukhtar <Zeshanmukhtar0878@gmail.com>
2024-01-15 19:04:17 -0800

    Refactor code in App.jsx and Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,46 +1,50 @@
-import React, {useEffect, useState} from 'react'
-import appwriteService from "../appwrite/config";
-import {Container, PostCard} from '../components'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Container, PostCard, Button } from '../components';
+import appwriteService from '../appwrite/config';
 
 function Home() {
-    const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
 
-    useEffect(() => {
-        appwriteService.getPosts().then((posts) => {
-            if (posts) {
-                setPosts(posts.documents)
-            }
-        })
-    }, [])
-  
-    if (posts.length === 0) {
-        return (
-            <div className="w-full py-8 mt-4 text-center">
-                <Container>
-                    <div className="flex flex-wrap">
-                        <div className="p-2 w-full">
-                            <h1 className="text-2xl font-bold hover:text-gray-500">
-                                Login to read posts
-                            </h1>
-                        </div>
-                    </div>
-                </Container>
-            </div>
-        )
-    }
-    return (
-        <div className='w-full py-8'>
-            <Container>
-                <div className='flex flex-wrap'>
-                    {posts.map((post) => (
-                        <div key={post.$id} className='p-2 w-1/4'>
-                            <PostCard {...post} />
-                        </div>
-                    ))}
-                </div>
-            </Container>
-        </div>
-    )
+  useEffect(() => {
+    appwriteService.getPosts().then((result) => {
+      if (result) {
+        setPosts(result.documents);
+      }
+    });
+  }, []);
+
+  return (
+    <div className="w-full py-8">
+      <Container>
+        {posts.length === 0 ? (
+          <div className="flex flex-col items-center justify-center h-full py-16">
+            <h1 className="text-4xl font-bold mb-4 text-center">
+              Explore the World of Posts
+            </h1>
+            <p className="text-lg mb-8 text-gray-600">
+              Log in to read and discover amazing posts. If you don't have an
+              account, you can{' '}
+              <Link to="/signup" className="text-primary hover:underline">
+                sign up here.
+              </Link>
+            </p>
+            <Link to="/login">
+              <Button>Log In</Button>
+            </Link>
+          </div>
+        ) : (
+          <div className="flex flex-wrap">
+            {posts.map((post) => (
+              <div key={post.$id} className="p-2 w-1/4">
+                <PostCard {...post} />
+              </div>
+            ))}
+          </div>
+        )}
+      </Container>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
